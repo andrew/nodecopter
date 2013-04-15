1ebf349b4d2e3626c20d1b416f007e0ed80af7f0
Andrew Nesbitt <andrewnez@gmail.com>
2013-04-15 11:26:18 +0100

    Simplified organization of commands

---
+++ nodecopter.js
@@ -0,0 +1,28 @@
+var Nodecopter = {
+  config: function(options){
+    console.log('config')
+  },
+  demo: function(options){
+    console.log('demo')
+  },
+  land: function(options){
+    console.log('land')
+  },
+  repl: function(options){
+    console.log('repl')
+  },
+  reset: function(options){
+    console.log('reset')
+  },
+  status: function(options){
+    console.log('status')
+  },
+  telnet: function(options){
+    console.log('telnet')
+  },
+  trim: function(options){
+    console.log('trim')
+  }
+}
+
+module.exports = Nodecopter
